ebab460b3fc218aa403bb934579eeb851ca7a86f
khaledrazouk <94979673+khaledrazouk@users.noreply.github.com>
2022-11-03 11:30:37 +0100

    static webapp

--- main.tf
+++ main.tf
@@ -3,33 +3,38 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
 }
 
-resource "azurerm_service_plan" "plan" {
-  name                = var.app_service_plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Windows"
-  sku_name            = "S1"
-}
-
-resource "azurerm_windows_web_app" "app" {
+resource "azurerm_static_site" "example" {
   name                = var.app_service_name
-  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.plan.id
-
-  site_config {
-    always_on = false
-
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-  }
+  location            = azurerm_resource_group.rg.location
 }
+# resource "azurerm_service_plan" "plan" {
+#   name                = var.app_service_plan_name
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   os_type             = "Windows"
+#   sku_name            = "S1"
+# }
+
+# resource "azurerm_windows_web_app" "app" {
+#   name                = var.app_service_name
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   service_plan_id     = azurerm_service_plan.plan.id
+
+#   site_config {
+#     always_on = false
+
+#   }
+
+#   app_settings = {
+#     "SOME_KEY" = "some-value"
+#   }
+
+#   connection_string {
+#     name  = "Database"
+#     type  = "SQLServer"
+#     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+#   }
+# }
 
